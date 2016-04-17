16470766b107d0aa6f9966333270a2511445bf07
saix <saix@bins-macbook-pro.local>
2016-04-17 18:01:25 +0800

    Initial Commit

---
+++ CrazeMM/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  CrazeMM
+//
+//  Created by saix on 16/4/17.
+//  Copyright © 2016年 189. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
