3fc3b4a7a20d23355b6bf6109f7d6028fe03c168
saix <saix@saix-mbp.citrite.net>
2016-05-04 22:40:02 +0800

    add coredata modal

---
+++ CrazeMM/restful/HttpSupplyRequest.h
@@ -0,0 +1,13 @@
+//
+//  HttpSupplyRequest.h
+//  CrazeMM
+//
+//  Created by saix on 16/5/4.
+//  Copyright © 2016年 189. All rights reserved.
+//
+
+#import "BaseHttpRequest.h"
+
+@interface HttpSupplyRequest : BaseHttpRequest
+
+@end
