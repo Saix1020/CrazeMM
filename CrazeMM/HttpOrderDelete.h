63630affe9aacb614cbda696be4d33e687a017df
basara7 <kirax10a6@126.com>
2016-05-13 21:46:05 +0800

    latest code

---
+++ CrazeMM/HttpOrderDelete.h
@@ -0,0 +1,17 @@
+//
+//  HttpOrderDelete.h
+//  CrazeMM
+//
+//  Created by Mao Mao on 16/5/13.
+//  Copyright © 2016年 189. All rights reserved.
+//
+
+#import "BaseHttpRequest.h"
+
+@interface HttpOrderDeleteRequest : BaseHttpRequest
+
+@end
+
+@interface HttpOrderDeleteResponse : BaseHttpResponse
+
+@end
