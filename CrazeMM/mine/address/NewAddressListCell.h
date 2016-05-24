958b49af41651761f74f5d3f010925a7d2f4dfdf
saix <saix@saix-mbp.citrite.net>
2016-05-24 13:59:59 +0800

    new app icon

---
+++ CrazeMM/mine/address/NewAddressListCell.h
@@ -0,0 +1,13 @@
+//
+//  NewAddressListCell.h
+//  CrazeMM
+//
+//  Created by saix on 16/5/24.
+//  Copyright © 2016年 189. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface NewAddressListCell : UITableViewCell
+
+@end
